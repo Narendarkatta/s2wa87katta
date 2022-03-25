a23c2637d651fa4c87a9682819299de6737ae403
Narendarkatta <k.narendarreddy111@gmail.com>
2022-03-24 19:27:10 -0500

    new endpoint in main branch

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('mydata', { title: 'Narendar reddy katta',fact: 'I am good at Badminton' });
 });
 
 module.exports = router;
